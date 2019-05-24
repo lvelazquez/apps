0114beaae7a0bfaf378433e3f48a8a3828144761
Leonardo Velázquez <v-leve@microsoft.com>
2019-05-24 15:47:09 -0700

    drawing in canvas

--- noc/src/point.ts
+++ noc/src/point.ts
@@ -1,6 +1,6 @@
-class Point {
-  private x: number;
-  private y: number;
+export default class Point {
+  public x: number;
+  public y: number;
 
   constructor(x: number = 0, y: number = 0) {
     this.x = x;
--- noc/src/randomwalk.ts
+++ noc/src/randomwalk.ts
@@ -1,21 +1,39 @@
+import Point from "./point";
+
 export class RandomWalk {
   private requestId: number;
-  private isRunning: boolean;
-  private cvn: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+  private pen: Point;
 
   constructor() {
-    this.isRunning = false;
-    this.cvn = document.getElementsByTagName("canvas")[0];
+    this.requestId = 0;
+    const cvn: HTMLCanvasElement = document.querySelector(
+      "canvas"
+    ) as HTMLCanvasElement;
+    this.ctx = cvn.getContext("2d") as CanvasRenderingContext2D;
+    this.ctx.strokeStyle = "#FF0000";
+    this.pen = new Point(0, 0);
+    this.ctx.moveTo(0, 0);
   }
 
   start() {
     this.loop();
   }
   stop() {
+    this.ctx.moveTo(0, 0);
     window.cancelAnimationFrame(this.requestId);
   }
   loop = (): void => {
     this.requestId = window.requestAnimationFrame(this.loop);
-    console.log("loop");
+    const speed = 1;
+
+    if (Math.round(Math.random()) * 10 > 5) {
+      this.pen.x += speed;
+    } else {
+      this.pen.y += speed;
+    }
+    console.log(this.pen);
+
+    this.ctx.lineTo(this.pen.x, this.pen.y);
   };
 }
